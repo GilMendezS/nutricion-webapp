493e8f4382a3b550afb8a643acf44fa3ac1ea3c6
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-07-08 14:15:25 -0500

    new seeder to create first roles

---
+++ database/seeds/RoleSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Role;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create([
+            'code' => 'nut',
+            'name' => 'Nutriologo'
+        ]);
+        Role::create([
+            'code' => 'pac',
+            'name' => 'Paciente'
+        ]);
+    }
+}
