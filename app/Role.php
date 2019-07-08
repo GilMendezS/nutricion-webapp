af5952aa844561c244cc316fc3ad9e0fdf77bc59
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-07-08 14:14:50 -0500

    relationships defined

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    const NUTRIOLOGIST = 1;
+    const PATIENT = 2;
+    protected $table = 'roles';
+    public function users(){
+        return $this->belongsToMany(User::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Role;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -36,4 +37,22 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function getPacients(){
+        return $this->hasMany(User::class, 'creator_id', 'id')->where('creator_id', $this->id);
+    }
+    public function becomesPatient(){
+        return $this->roles()->attach(Role::PATIENT);
+    }
+    public function becomesNutrioligist(){
+        return $this->roles()->attach(ROle::NUTRIOLOGIST);
+    }
+    public function hasRole($role){
+        return in_array($role, $this->roles->pluck('id')->toArray());
+    }
+    public function removeRole($role){
+        return $this->roles()->detach($role);
+    }
+    public function roles(){
+        return $this->belongsToMany(Role::class);
+    }
 }
